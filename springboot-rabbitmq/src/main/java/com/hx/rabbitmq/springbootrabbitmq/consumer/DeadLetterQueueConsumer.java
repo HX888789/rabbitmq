cb35c67fbf5a7503e5f15dd320b1d2776a99bda3
HX <80191207+HX888789@users.noreply.github.com>
2021-08-12 21:05:09 +0800

    springboot RabbitMq

---
+++ springboot-rabbitmq/src/main/java/com/hx/rabbitmq/springbootrabbitmq/consumer/DeadLetterQueueConsumer.java
@@ -0,0 +1,30 @@
+package com.hx.rabbitmq.springbootrabbitmq.consumer;
+
+import com.rabbitmq.client.Channel;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+
+/**
+ * 队列ttl 消费者
+ */
+@Slf4j
+@Component
+public class DeadLetterQueueConsumer {
+
+    /**
+     * @param message 消息
+     * @param channel 信道
+     * @throws Exception 异常
+     */
+    @RabbitListener(queues = "QD")
+    public void receiveD(Message message, Channel channel)throws Exception{
+        String msg = String.valueOf(message.getBody());
+        log.info("当前时间：{},收到死信队列消息：{}",new Date().toString(),msg);
+    }
+
+}
